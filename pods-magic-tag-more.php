8d743c44d02d28bf14f5b695054a661f53cfcc53
NikV <techvoltz@gmail.com>
2015-01-28 06:46:21 -0600

    First commit

---
+++ pods-magic-tag-more.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Plugin Name: Pods Magic Tag More
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: A brief description of the Plugin.
+ * Version: 1.0
+ * Author: nikhil
+ * Author URI: http://URI_Of_The_Plugin_Author
+ * License: A "Slug" license name e.g. GPL2
+*/
+
+function pods_more_stff_things_more($code = null, $this) {
+
+	global $post;
+
+	$content = get_post_field( 'post_content', $post->ID );
+	$get_extended = get_extended($content);
+	$code = str_replace( '{@post_more}', $get_extended['main'], $code );
+
+	return $code;
+}
+add_filter('pods_templates_pre_template', 'pods_more_stff_things_more', 10, 2);
+
+
+
