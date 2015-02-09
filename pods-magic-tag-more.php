75cbd4fd1ecef48c818b8aa95c61c8467062e0bc
NikV <techvoltz@gmail.com>
2015-02-08 21:05:43 -0600

    FIX

--- pods-magic-tag-more.php
+++ pods-magic-tag-more.php
@@ -10,24 +10,30 @@
  * License: A "Slug" license name e.g. GPL2
 */
 
-function pods_more_stff_things_more($code, $template_name,  $pods) {
+/**
+ * The function that register the magic tag
+ *
+ * @param $code
+ * @param $template_name
+ * @param $pods
+ */
+function pods_post_more_magic_tag($code, $template_name,  $pods) {
 
-	if ( strstr( get_post_field( 'post_content', $pods->ID()), '<!--more-->') ) {
 
-	$content      = get_post_field( 'post_content', $pods->ID() );
-	$get_extended = get_extended( $content );
-	$code         = str_replace( '{@post_more}', $get_extended['main'], $code );
+		//Will only run if the more tag exists
+		if ( strstr( get_post_field( 'post_content', $pods->ID() ), '<!--more-->' ) ) {
 
-	return $code;
+			$content      = get_post_field( 'post_content', $pods->ID() );
+			$get_extended = get_extended( $content );//get_extended() will get content before the more tag
+			$code         = str_replace( '{@post_more}', $get_extended['main'], $code );
 
-	}
+			return $code;
 
-	else {
-		echo get_the_excerpt();
-	}
+		} elseif ( ! strstr( get_post_field( 'post_content', $pods->ID() ), '<!--more-->' ) ) {
+			echo get_post_field( 'post_excerpt', $pods->ID() );
+		}
+
+
 
 }
-add_filter('pods_templates_pre_template', 'pods_more_stff_things_more', 10, 3);
-
-
-
+add_filter('pods_templates_pre_template', 'pods_post_more_magic_tag', 1, 3);
